0aea82674e217a5c407b0efaf3fd56ddcca5151f
Tim Schneider <tim.schneider1337@gmail.com>
2024-10-22 14:20:01 +0200

    Added clangd and FiraCode-Font. Formatting

--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,79 +1,81 @@
 { pkgs, ... }: {
-	nixpkgs.config = {
-		allowUnfree = true;
-	};
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
 
-	environment.systemPackages = with pkgs; [
-		#Desktop Apps
-		firefox
-		obsidian
-		zoom-us
+  environment.systemPackages = with pkgs; [
+    #Desktop Apps
+    firefox
+    obsidian
+    zoom-us
 
-		# Coding stuff
-		(python3.withPackages (ps: with ps; [ requests pyquery ]))
-		cachix
-		devenv
-		gcc
-		vim
+    # Coding stuff
+    (python3.withPackages (ps: with ps; [ requests pyquery ]))
+    cachix
+    devenv
+    gcc
+    clang-tools
+    vim
 
-		# CLI utils
-		bluez
-		bluez-tools
-		btop
-		fastfetch
-		file
-		git
-		htop
-		imv
-		lazygit
-		neofetch
-		tree
-		unzip
-		wget
-		zip
+    # CLI utils
+    bluez
+    bluez-tools
+    btop
+    fastfetch
+    file
+    git
+    htop
+    imv
+    lazygit
+    neofetch
+    tree
+    unzip
+    wget
+    zip
     fzf
 
-		# Wayland stauff
-		brightnessctl
+    # Wayland stauff
+    brightnessctl
     cliphist
-		libsForQt5.qt5.qtwayland
-		qt5.qtwayland
-		qt6.qtwayland
+    libsForQt5.qt5.qtwayland
+    qt5.qtwayland
+    qt6.qtwayland
     wl-clipboard
-		xwayland
+    xwayland
 
-		# WMs and stuff
-		dunst 		#Notifications
-		hypridle
-		hyprland
-		hyprlock
+    # WMs and stuff
+    dunst 		#Notifications
+    hypridle
+    hyprland
+    hyprlock
     hyprshot
-		seatd
-		swww 		  #Wallpaper
-		waybar
-		wofi
-		xdg-desktop-portal-hyprland
+    seatd
+    swww 		  #Wallpaper
+    waybar
+    wofi
+    xdg-desktop-portal-hyprland
 
-		# Sound
-		pamixer
-		pavucontrol
-		pipewire
-		pulseaudio
+    # Sound
+    pamixer
+    pavucontrol
+    pipewire
+    pulseaudio
 
-		#Other
-		enpass
-		home-manager
-		kitty
-		xdg-utils
-	];
+    #Other
+    enpass
+    home-manager
+    kitty
+    xdg-utils
+  ];
 
-	fonts.packages = with pkgs; [
-		jetbrains-mono
-		noto-fonts
-		noto-fonts-emoji
-		font-awesome
-		powerline-fonts
-		powerline-symbols
-		(nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
-	];
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-emoji
+    fira-code
+    fira-code-symbols
+    font-awesome
+    powerline-fonts
+    powerline-symbols
+    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" "FiraCode" ]; })
+  ];
 }
