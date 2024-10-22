1f2492c8ceda8bc814e1fc98632cc83bac502d0c
Tim Schneider <tim.schneider1337@gmail.com>
2024-10-22 11:10:58 +0200

    config change

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -3,5 +3,9 @@
 		enable = true;
 		userName = "Tim Schneider";
 		userEmail = "tim.schneider1337@gmail.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
 	};
 }
--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -22,6 +22,7 @@
 			ll = "ls -l";
 			v = "nvim";
 			se = "sudoedit";
+      lg = "lazygit";
 		};
 
 
